83c7fe48c6d55d6331c4dab69c0c91aa523fdec8
kasperzkid <kasperzkid@gmail.com>
2026-06-06 16:41:34 +0300

    chore: support VITE_BACKEND_URL for dynamic axios routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AuthProvider } from './contexts/AuthContext'
+import axios from 'axios'
 import './index.css'
 import App from './App.jsx'
+
+axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL || '';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
